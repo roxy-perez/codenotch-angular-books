7121e820ae034451e2e5767aba726a83dde290dc
Roxy <necropsia32@gmail.com>
2023-02-19 00:12:27 +0000

    Terminando de maquetar el día 1 de los retos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    RegisterFormComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
