8688fe3846d945930e3100458ea91c672ae81403
RoxyWTF <necropsia32@gmail.com>
2023-02-21 21:05:21 +0000

    Trying to implement AngularForms

---
+++ src/app/models/user.ts
@@ -0,0 +1,52 @@
+export class User {
+  constructor(
+    private _id: number,
+    private _name: string,
+    private _surname: string,
+    private _email: string,
+    private _url: string
+  ) {}
+
+  // Getters
+  public get id() {
+    return this._id;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get surname(): string {
+    return this._surname;
+  }
+
+  public get email(): string {
+    return this._email;
+  }
+
+  public get url(): string {
+    return this._url;
+  }
+
+  // Setters
+  public set id(id: number) {
+    this._id = id;
+  }
+
+  public set name(name: string) {
+    this._name = name;
+  }
+
+  public set surname(surname: string) {
+    this._surname = surname;
+  }
+
+  public set email(email: string) {
+    this._email = email;
+  }
+
+  public set url(url: string) {
+    this._url = url;
+  }
+}
+
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { User } from "src/app/models/user";
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"],
 })
 export class ProfileComponent {
+
+  // usuario de prueba
+  user = new User(
+    1,
+    "Roxy",
+    "Pérez",
+    "necropsia32@gmail.com",
+    "/images/user.jpg"
+  );
+
+  newUser = {} as User;
+
+  @ViewChild('itemForm', { static: false }) form!: NgForm;
+
+  onSubmit(form: NgForm) {
+    console.log(JSON.stringify(form.value));
+  }
 
 }
